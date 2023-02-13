1061fa574c69c0b44a0a057a21f4876f6ddb2a5d
bocill123 <125232719+bocill123@users.noreply.github.com>
2023-02-13 09:55:53 +0700

    Add files via upload

---
+++ wildan_tkj2/latihan_2.py
@@ -0,0 +1,10 @@
+def segitiga(alas,tinggi):
+    luas = alas * tinggi/2
+    print("luas degitiga adalah:%1 cm" %luas)
+segitiga(15,20)
+
+
+def persegi_panjang(panjang, lebar):
+    luas = panjang*lebar
+    print("luas bangun datar persegi panjang adalah: %icm" %luas)
+persegi_panjang(20,40)
---
+++ wildan_tkj2/latihan_fungsi.py
@@ -0,0 +1,53 @@
+'''Fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("assalamu'alaikum !! akhi ukhty")
+    
+salam()    
+salam()
+salam()
+
+'''Buatlah sebuah fungsi versi kalian'''
+def kalem():
+    print("why you beatifull")
+
+kalem()
+kalem()
+kalem()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("kebot")
+
+'''buatlah fungsi dengan 3 parameter'''
+
+def say_hi(nama):
+    print("perkenalkan nama saya", nama)
+
+say_hi("orang")
+
+def orang_ganteng(orang1, orang2, orang3):
+    print("orang ganteng adalah"+ " "+ orang3)
+
+orang_ganteng(orang1="gua", orang2="luuh", orang3="kamu")
+
+
+def perkenalan(nama, alamat, umur):
+    print("nama saya:",nama)
+    print("alamat saya:",alamat)
+    print("umur saya:",umur)
+
+
+perkenalan("kebot","depok","kamu nnya")
+
+
+
+
+
